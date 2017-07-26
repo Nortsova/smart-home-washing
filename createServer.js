10b045753b00dd92e8dba3451f5dafaccc3953fc
anka-kmbl <anka.kmbl@mail.ru>
2017-07-26 20:40:34 +0300

    Delete createServer.js

--- createServer.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-var url = require('url');
-
-http.createServer(function(req, res) {
-	res.writeHead(200, {"Content-Type": "text/html"});
-	res.write("Hello smarthome!!");
-	
-	res.end();
-}).listen(9000);
