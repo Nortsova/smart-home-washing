0fea7ec7b811306a691ff1c5b5f0990da7f5b64e
anna.maksymenko@nure.ua <anna.maksymenko@nure.ua>
2017-07-27 14:58:05 +0300

    changed substing to substr

--- firstTRY/read.js
+++ firstTRY/read.js
@@ -2,11 +2,44 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 var path = require('path');
+var mimeTypes = {
+	'.js': 'text/javascript',
+	'.html': 'text/html',
+	'.css': 'text/css',
+	'.jpg': 'image/jpeg',
+	'.png': 'image/png'
+
+};
+
+
 http.createServer(function (req, res) {
-	var pathname = url.parse(req.url).pathname;
+	var pathname = url.parse(req.url).path;
 	var ext = path.extname(pathname);
 
-	 if(ext){
+	if(pathname == '/') {
+		pathname = '/hello-smarthouse.html';
+	}
+
+	var mimeType = mimeTypes[ext];
+	pathname = pathname.substr(1);
+
+	if((ext == ".png") || (ext == ".jpg")) {
+		fs.readFile('./' + pathname, function(err, data) {
+			res.writeHead(200, {'Content-Type': mimeType});
+			
+			res.end(data, 'binary');
+		})
+	} else {
+		fs.readFile(pathname, 'utf8', function (err, data) {
+			if(err) console.log("err");
+			else {
+				res.writeHead(200, {'Content-Type': mimeType});
+				res.end(data);
+			}
+		})
+	}
+
+	 /*if(ext){
         if(ext === '.css'){
             res.writeHead(200, {'Content-Type': 'text/css'});
         }
@@ -28,7 +61,7 @@ http.createServer(function (req, res) {
             	res.end();
 
             });
-     }
+     }*/
      //res.end();
      
  /* fs.readFile('hello-smarthouse.html', function(err, data) {
