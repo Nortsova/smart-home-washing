71bdf5b1398244597b1a880cb5ba58fca601315d
Anna Maksymenko <anna.maksymenko@nure.ua>
2017-07-27 19:36:03 +0300

    now an image can be added (changed read.js file)

--- firstTRY/read.js
+++ firstTRY/read.js
@@ -2,38 +2,43 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 var path = require('path');
+var mimeTypes = {
+    '.js': 'text/javascript',
+    '.html': 'text/html',
+    '.css': 'text/css',
+    '.jpg': 'image/jpeg',
+    '.png': 'image/png'
+};
+
+
 http.createServer(function (req, res) {
 	var pathname = url.parse(req.url).pathname;
-	var ext = path.extname(pathname);
+	
+    if(pathname == '/') {
+        pathname = '/index.html'
+    }
 
-	 if(ext){
-        if(ext === '.css'){
-            res.writeHead(200, {'Content-Type': 'text/css'});
-        }
-        else if(ext === '.js'){
-            res.writeHead(200, {'Content-Type': 'text/javascript'});
-        }
-            fs.readFile(__dirname + pathname, 'utf8', function(err, data) {
-            	if(err) throw err;
-            	res.write(data);
-            	res.end();
+    var ext = path.extname(pathname);
+    
+    var mimeType = mimeTypes[ext];
+    pathname = pathname.substring(1, pathname.length);
 
-            });
-        }
-     else{
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            fs.readFile('hello-smarthouse.html', 'utf8', function(err, data) {
-            	if(err) throw err;
-            	res.write(data);
-            	res.end();
+    if((ext == ".png") || (ext == ".jpg")) {
+        fs.readFile('./' + pathname, function(err, data) {
+            res.writeHead(200, {'Content-Type': mimeType});
+            res.end(data, 'binary');
+        });
+        
+    } else {
+        fs.readFile(pathname, 'utf8', function(err, data) {
+            res.writeHead(200, {'Content-Type': mimeType});
+            res.end(data);
+        });
+    }
 
-            });
-     }
-     //res.end();
-     
- /* fs.readFile('hello-smarthouse.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    res.end();
-  });*/
+
+
+    
+
 }).listen(8080);
+
