ad00b6b2ae1b83dc9691435d7005879373d62043
Anna Maksymenko <anna.maksymenko@nure.ua>
2017-07-28 12:43:45 +0300

    added dependence on a method

--- firstTRY/read.js
+++ firstTRY/read.js
@@ -14,31 +14,33 @@ var mimeTypes = {
 
 
 http.createServer(function (req, res) {
+    if(req.method == "GET") {
+    	var pathname = url.parse(req.url).path;
+    	var ext = path.extname(pathname);
 
-	var pathname = url.parse(req.url).path;
-	var ext = path.extname(pathname);
+    	if(pathname == '/') {
+    		pathname = '/index.html';
+    	}
 
-	if(pathname == '/') {
-		pathname = '/index.html';
-	}
+    	var mimeType = mimeTypes[ext];
+    	pathname = pathname.substr(1);
 
-	var mimeType = mimeTypes[ext];
-	pathname = pathname.substr(1);
-
-	if((ext == ".png") || (ext == ".jpg")) {
-		fs.readFile('./' + pathname, function(err, data) {
-			res.writeHead(200, {'Content-Type': mimeType});
-			
-			res.end(data, 'binary');
-		})
-	} else {
-		fs.readFile(pathname, 'utf8', function (err, data) {
-			if(err) res.end("<h1 style='text-align:center; margin: 100px 0;'>ERROR 404 (Not found)</h1>");
-			else {
-				res.writeHead(200, {'Content-Type': mimeType});
-				res.end(data);
-			}
-		})
-	}
-
+    	if((ext == ".png") || (ext == ".jpg")) {
+    		fs.readFile('./' + pathname, function(err, data) {
+    			res.writeHead(200, {'Content-Type': mimeType});
+    			
+    			res.end(data, 'binary');
+    		})
+    	} else {
+    		fs.readFile(pathname, 'utf8', function (err, data) {
+    			if(err) res.end("<h1 style='text-align:center; margin: 100px 0;'>ERROR 404 (Not found)</h1>");
+    			else {
+    				res.writeHead(200, {'Content-Type': mimeType});
+    				res.end(data);
+    			}
+    		})
+    	}
+    } else {
+        res.end("<h1>wrong method</h1>")
+    }
 }).listen(8080);
