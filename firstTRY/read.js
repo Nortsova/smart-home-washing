7872e59945241310bc05f3b5ab7bcaf928759c51
anna.maksymenko@nure.ua <anna.maksymenko@nure.ua>
2017-07-28 10:08:32 +0300

    added error 404 not found

--- firstTRY/read.js
+++ firstTRY/read.js
@@ -3,30 +3,23 @@ var fs = require('fs');
 var url = require('url');
 var path = require('path');
 var mimeTypes = {
-<<<<<<< HEAD
+
 	'.js': 'text/javascript',
 	'.html': 'text/html',
 	'.css': 'text/css',
 	'.jpg': 'image/jpeg',
 	'.png': 'image/png'
 
-=======
-    '.js': 'text/javascript',
-    '.html': 'text/html',
-    '.css': 'text/css',
-    '.jpg': 'image/jpeg',
-    '.png': 'image/png'
->>>>>>> 71bdf5b1398244597b1a880cb5ba58fca601315d
 };
 
 
 http.createServer(function (req, res) {
-<<<<<<< HEAD
+
 	var pathname = url.parse(req.url).path;
 	var ext = path.extname(pathname);
 
 	if(pathname == '/') {
-		pathname = '/hello-smarthouse.html';
+		pathname = '/index.html';
 	}
 
 	var mimeType = mimeTypes[ext];
@@ -40,7 +33,7 @@ http.createServer(function (req, res) {
 		})
 	} else {
 		fs.readFile(pathname, 'utf8', function (err, data) {
-			if(err) console.log("err");
+			if(err) res.end("<h1 style='text-align:center; margin: 100px 0;'>ERROR 404 (Not found)</h1>");
 			else {
 				res.writeHead(200, {'Content-Type': mimeType});
 				res.end(data);
@@ -48,66 +41,4 @@ http.createServer(function (req, res) {
 		})
 	}
 
-	 /*if(ext){
-        if(ext === '.css'){
-            res.writeHead(200, {'Content-Type': 'text/css'});
-        }
-        else if(ext === '.js'){
-            res.writeHead(200, {'Content-Type': 'text/javascript'});
-        }
-            fs.readFile(__dirname + pathname, 'utf8', function(err, data) {
-            	if(err) throw err;
-            	res.write(data);
-            	res.end();
-
-            });
-        }
-     else{
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            fs.readFile('hello-smarthouse.html', 'utf8', function(err, data) {
-            	if(err) throw err;
-            	res.write(data);
-            	res.end();
-
-            });
-     }*/
-     //res.end();
-     
- /* fs.readFile('hello-smarthouse.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    res.end();
-  });*/
 }).listen(8080);
-=======
-	var pathname = url.parse(req.url).pathname;
-	
-    if(pathname == '/') {
-        pathname = '/index.html'
-    }
-
-    var ext = path.extname(pathname);
-    
-    var mimeType = mimeTypes[ext];
-    pathname = pathname.substring(1, pathname.length);
-
-    if((ext == ".png") || (ext == ".jpg")) {
-        fs.readFile('./' + pathname, function(err, data) {
-            res.writeHead(200, {'Content-Type': mimeType});
-            res.end(data, 'binary');
-        });
-        
-    } else {
-        fs.readFile(pathname, 'utf8', function(err, data) {
-            res.writeHead(200, {'Content-Type': mimeType});
-            res.end(data);
-        });
-    }
-
-
-
-    
-
-}).listen(8080);
-
->>>>>>> 71bdf5b1398244597b1a880cb5ba58fca601315d
