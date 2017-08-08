3398b096ac78f58a5a53f257f6200afb6bb22c4c
Anastasia <anastasi.smch@gmail.com>
2017-08-08 14:44:25 +0300

    delete console.log and formatting code

--- washmashineWeek2/app.js
+++ washmashineWeek2/app.js
@@ -7,64 +7,65 @@ let favicon = require('serve-favicon');
 //let handlebars = require('handlebars');
 let app = express();
 app.set('view engine', 'pug');
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/views'));
 app.use(favicon(__dirname + '/public/favicon.ico'));
 let info;
+
 app.get('/', (req, res) => {
 	fs.readFile('./data.json', (err, data) => {
-  	if(err) console.log(err);
-	  	info = JSON.parse(data);
-	  	let changeL = info.changeLog;
-	  	if(changeL) {
-	  		let swClass;
-	  		let drClass;
-	  		if(changeL.switchClass) {
-	  			swClass = changeL.switchClass;
-	  			//drClass = changeL.drumClass;
-	  		} else {
-	  			changeL.drumClass = 'open';
-	  			drClass = changeL.drumClass;
-	  		}
-	  		//let addModeMenu;
-	  		if(!changeL.addModeApp) {
-	  			changeL.addModeApp = 'none';
-	  		} 
-	  		let modeArr;
-	  		if(!changeL.modes) {
-	  			modeArr = [];
-	  		} else {
-	  			modeArr = changeL.modes;
-	  		}
-	  		//let changeModeMenu;
-	  		if(!changeL.changeModeApp) {
-	  			changeL.changeModeApp = 'none';
-	  		} 
-	  		
-	  		if(!changeL.modesDivApp) {
-	  			changeL.modesDivApp = 'none';
-	  		}
-
-	  		res.render('index', {welcDivApp: changeL.welcDivApp,
-	  		 	formApp: changeL.formApp, welcText: changeL.welcText,
-	  			switchClass: swClass, drumClass: drClass,
-	  			modesDivApp: changeL.modesDivApp, addModeApp: changeL.addModeApp,
-	  			modes: modeArr, changeModeApp: changeL.changeModeApp,
-	  			modeProps: changeL.modeProps})
-	  	} else {
-	  		res.render('index', {welcDivApp: 'none', formApp: 'block',
-	  			drumClass: 'open', modesDivApp: 'none', addModeApp: 'none',
-	  			modes: [], changeModeApp: 'none'});
-	  	}
-  })
+		if (err) console.log(err);
+		info = JSON.parse(data);
+		let changeL = info.changeLog;
+		if (changeL) {
+			let swClass;
+			let drClass;
+			if (changeL.switchClass) {
+				swClass = changeL.switchClass;
+			} else {
+				changeL.drumClass = 'open';
+				drClass = changeL.drumClass;
+			}
+			if (!changeL.addModeApp) {
+				changeL.addModeApp = 'none';
+			}
+			let modeArr;
+			if (!changeL.modes) {
+				modeArr = [];
+			} else {
+				modeArr = changeL.modes;
+			}
+			if (!changeL.changeModeApp) {
+				changeL.changeModeApp = 'none';
+			}
+			if (!changeL.modesDivApp) {
+				changeL.modesDivApp = 'none';
+			}
+			res.render('index', {
+				welcDivApp: changeL.welcDivApp,
+				formApp: changeL.formApp, welcText: changeL.welcText,
+				switchClass: swClass, drumClass: drClass,
+				modesDivApp: changeL.modesDivApp, addModeApp: changeL.addModeApp,
+				modes: modeArr, changeModeApp: changeL.changeModeApp,
+				modeProps: changeL.modeProps
+			})
+		} else {
+			res.render('index', {
+				welcDivApp: 'none', formApp: 'block',
+				drumClass: 'open', modesDivApp: 'none', addModeApp: 'none',
+				modes: [], changeModeApp: 'none'
+			});
+		}
+	})
 })
+
 app.put('/logIn', (req, res) => {
 	fs.readFile('./data.json', (err, data) => {
-		if(err) console.log(err);
+		if (err) console.log(err);
 		info = JSON.parse(data);
-		console.log(info);
-		if(info.ok) {
+		//console.log(info);
+		if (info.ok) {
 			console.log("server ok!");
 			info.changeLog = {};
 			let changeL = info.changeLog;
@@ -73,23 +74,26 @@ app.put('/logIn', (req, res) => {
 			changeL.welcDivApp = 'block';
 			info.userName = req.body.userName;
 			saveChanges(info, callBack());
-			res.send(JSON.stringify({welcText: changeL.welcText,
-				formApp: changeL.formApp, welcDivApp: changeL.welcDivApp}));
+			res.send(JSON.stringify({
+				welcText: changeL.welcText,
+				formApp: changeL.formApp, welcDivApp: changeL.welcDivApp
+			}));
 		};
 	})
-});
+})
+
 app.put('/switchDevice', (req, res) => {
 	fs.readFile('./data.json', (err, data) => {
-		if(err) console.log(err);
+		if (err) console.log(err);
 		info = JSON.parse(data);
-		if(!info.changeLog) {
-			res.send(JSON.stringify({notLogged: true}));
+		if (!info.changeLog) {
+			res.send(JSON.stringify({ notLogged: true }));
 		} else {
 			let changeL = info.changeLog;
 			let classOpen = 'open';
 			let classOn = 'on';
 
-			if(req.body.switchedOn) {
+			if (req.body.switchedOn) {
 				changeL.switchClass = false;
 				changeL.drumClass = 'open';
 			} else {
@@ -97,71 +101,76 @@ app.put('/switchDevice', (req, res) => {
 				changeL.drumClass = false;
 			}
 			saveChanges(info, callBack());
-			res.send(JSON.stringify({switchClass: changeL.switchClass,
-				drumClass: changeL.drumClass, open: classOpen, on: classOn}));
+			res.send(JSON.stringify({
+				switchClass: changeL.switchClass,
+				drumClass: changeL.drumClass, open: classOpen, on: classOn
+			}));
 		}
 	})
 })
 
 app.get('/logOut', (req, res) => {
 	fs.readFile('./data.json', (err, data) => {
-		if(err) console.log(err);
+		if (err) console.log(err);
 		info = JSON.parse(data);
-	    info.changeLog = '';
+		info.changeLog = '';
 
-	    
-	    res.send(JSON.stringify({welcDivApp: 'none', formApp: 'block',
-	    	modesDivApp: 'none', userName: info.userName, addModeApp: 'none',
-			changeModeApp: 'none'}));
-	    info.userName = '';
-	    saveChanges(info, callBack());
-
+		res.send(JSON.stringify({
+			welcDivApp: 'none', formApp: 'block',
+			modesDivApp: 'none', userName: info.userName, addModeApp: 'none',
+			changeModeApp: 'none'
+		}));
+		info.userName = '';
+		saveChanges(info, callBack());
 	})
 })
 
 app.get('/api/getModes', (req, res) => {
 	fs.readFile('./data.json', (err, data) => {
-		if(err) console.log(err);
+		if (err) console.log(err);
 		info = JSON.parse(data);
-		if(!info.changeLog) {
-			res.send(JSON.stringify({notLogged: true}));
+		if (!info.changeLog) {
+			res.send(JSON.stringify({ notLogged: true }));
 		} else {
 			let changeL = info.changeLog;
-			if(changeL.modesDivApp == 'block') {
+			if (changeL.modesDivApp == 'block') {
 				changeL.modesDivApp = 'none';
 				changeL.addModeApp = 'none';
-				res.send({modesDivApp: changeL.modesDivApp,
-					addModeApp: changeL.addModeApp});
+				res.send({
+					modesDivApp: changeL.modesDivApp,
+					addModeApp: changeL.addModeApp
+				});
 			} else {
 				changeL.modesDivApp = 'block';
-				res.send(JSON.stringify({modesDivApp: changeL.modesDivApp}));
+				res.send(JSON.stringify({ modesDivApp: changeL.modesDivApp }));
 			}
 			saveChanges(info, callBack());
-			
-		}	
+		}
 	})
 })
 
 app.get('/api/addModeMenu', (req, res) => {
 	fs.readFile('./data.json', (err, data) => {
-		if(err) console.log(err);
+		if (err) console.log(err);
 		info = JSON.parse(data);
 		let changeL = info.changeLog;
 		changeL.addModeApp = 'block';
 		changeL.changeModeApp = 'none';
 		saveChanges(info, callBack());
-		res.send(JSON.stringify({addModeApp: changeL.addModeApp,
-			changeModeApp: changeL.changeModeApp}));
-	})	
+		res.send(JSON.stringify({
+			addModeApp: changeL.addModeApp,
+			changeModeApp: changeL.changeModeApp
+		}));
+	})
 })
 app.post('/api/createMode', (req, res) => {
 	fs.readFile('./data.json', (err, data) => {
-		if(err) console.log(err);
+		if (err) console.log(err);
 		info = JSON.parse(data);
 		let changeL = info.changeLog;
-		if(!changeL.modes) {
+		if (!changeL.modes) {
 			changeL.modes = [];
-		} 
+		}
 		let modes = changeL.modes;
 		modes.push({});
 		modes[modes.length - 1].modeName = req.body.modeName;
@@ -169,42 +178,41 @@ app.post('/api/createMode', (req, res) => {
 		modes[modes.length - 1].temp = req.body.temp;
 		modes[modes.length - 1].id = Math.floor(Math.random() * 1000);
 		changeL.addModeApp = 'none';
-	 
+
 		saveChanges(info, callBack());
-		res.send(JSON.stringify({modes: changeL.modes, addModeApp: changeL.addModeApp }));
+		res.send(JSON.stringify({ modes: changeL.modes, addModeApp: changeL.addModeApp }));
 	})
 });
 
 app.get('/api/closeCreateMode', (req, res) => {
-    fs.readFile('./data.json', (err, data) => {
-        if(err) console.log(err);
-        info = JSON.parse(data);
-        let changeL = info.changeLog;
-        changeL.addModeApp = 'none';
-        saveChanges(info, callBack());
-		res.send(JSON.stringify({addModeApp: changeL.addModeApp}));
+	fs.readFile('./data.json', (err, data) => {
+		if (err) console.log(err);
+		info = JSON.parse(data);
+		let changeL = info.changeLog;
+		changeL.addModeApp = 'none';
+		saveChanges(info, callBack());
+		res.send(JSON.stringify({ addModeApp: changeL.addModeApp }));
 	})
 })
-//let modeToChange;
+
 app.get('/api/changeModeMenu', (req, res) => {
 	fs.readFile('./data.json', (err, data) => {
-		if(err) console.log(err);
+		if (err) console.log(err);
 		info = JSON.parse(data);
 		let changeL = info.changeLog;
 		let modes = changeL.modes;
-		//let modeId = url.parse(req.url).query.modeId;
 		let modeToChange;
-		for(let i = 0; i < modes.length; i++) {
-			if(modes[i].id == req.query.modeId) {
+		for (let i = 0; i < modes.length; i++) {
+			if (modes[i].id == req.query.modeId) {
 				modeToChange = modes[i];
 				break;
 			}
 		}
-		console.log(modeToChange);
+		//console.log(modeToChange);
 		changeL.changeModeApp = 'block';
 		changeL.addModeApp = 'none';
 		let modeProps;
-		if(!changeL.modeProps) {
+		if (!changeL.modeProps) {
 			changeL.modeProps = {};
 		}
 		modeProps = changeL.modeProps;
@@ -213,22 +221,25 @@ app.get('/api/changeModeMenu', (req, res) => {
 		modeProps.temp = modeToChange.temp;
 		modeProps.id = modeToChange.id;
 		saveChanges(info, callBack());
-		res.send(JSON.stringify({changeModeApp: changeL.changeModeApp,
+		res.send(JSON.stringify({
+			changeModeApp: changeL.changeModeApp,
 			modeName: modeProps.modeName, spin: modeProps.spin,
-			temp: modeProps.temp, addModeApp: changeL.addModeApp}));
+			temp: modeProps.temp, addModeApp: changeL.addModeApp
+		}));
 	})
 })
+
 app.put('/api/changeMode', (req, res) => {
 	fs.readFile('./data.json', (err, data) => {
-		if(err) console.log(err);
+		if (err) console.log(err);
 		info = JSON.parse(data);
 		let changeL = info.changeLog;
 		let modes = changeL.modes;
 		let modeProps = changeL.modeProps;
 		let modeToChange;
 		changeL.changeModeApp = 'none';
-		for(let i = 0; i < modes.length; i++) {
-			if(modes[i].id == modeProps.id) {
+		for (let i = 0; i < modes.length; i++) {
+			if (modes[i].id == modeProps.id) {
 				modeToChange = modes[i];
 				break;
 			}
@@ -237,35 +248,34 @@ app.put('/api/changeMode', (req, res) => {
 		modeToChange.spin = req.body.spin;
 		modeToChange.temp = req.body.temp;
 		saveChanges(info, callBack());
-		res.send(JSON.stringify({changeModeApp: changeL.changeModeApp,
-			modeToChange: modeToChange}))
-
+		res.send(JSON.stringify({
+			changeModeApp: changeL.changeModeApp,
+			modeToChange: modeToChange
+		}))
 	})
 })
 
 app.delete('/api/deleteMode', (req, res) => {
 	fs.readFile('./data.json', (err, data) => {
-		if(err) console.log(err);
+		if (err) console.log(err);
 		info = JSON.parse(data);
 		let changeL = info.changeLog;
 		let modes = changeL.modes;
 		//modeId;
-		console.log(modes);
+		// console.log(modes);
 		let index;
-		for(let i = 0; i < modes.length; i++) {
-			if(modes[i].id == req.body.modeId) {
-				console.log(modes[i]);
-				console.log(i);
+		for (let i = 0; i < modes.length; i++) {
+			if (modes[i].id == req.body.modeId) {
+				// console.log(modes[i]);
+				// console.log(i);
 				index = i;
 				break;
 			}
 		}
 		modes.splice(index, 1);
-		console.log(modes);
+		//console.log(modes);
 		saveChanges(info, callBack());
-		res.send(JSON.stringify({success: true}));
-
-
+		res.send(JSON.stringify({ success: true }));
 	})
 })
 
@@ -273,10 +283,9 @@ function saveChanges(file, callback) {
 	fs.writeFile('./data.json', JSON.stringify(file), callback);
 }
 
-
 function callBack() {
-	return function(err) {
-		if(err) {
+	return function (err) {
+		if (err) {
 			res.status(404).send('User data is not saved');
 			return;
 		}
--- washmashineWeek2/views/scripts/script.js
+++ washmashineWeek2/views/scripts/script.js
@@ -11,16 +11,13 @@ let addModeClose = document.getElementById('addModeClose');
 let closeAddModeForm = document.getElementById('closeAddModeForm');
 let openAddMode = document.getElementById('addModeB');
 let addModeDiv = document.getElementById('addMode');
-let addModeF = document.getElementById('addModeF');
+let addModeForm = document.getElementById('addModeForm');
 let modesTable = document.getElementById('modesTable');
-//let valSpin = document.getElementById('valSpin');
-//let valTemp = document.getElementById('valTemp');
-//let openChangeMode = document.getElementById('');
 let changeModeDiv = document.getElementById('changeMode');
 let changeModeF = document.getElementById('changeModeF');
-form.onsubmit = function(e) {
+
+form.onsubmit = function (e) {
 	e.preventDefault();
-	//alert('try to submit');
 	let data = JSON.stringify(formDataToJSON(form));
 	fetch('/logIn', {
 		headers: {
@@ -30,57 +27,54 @@ form.onsubmit = function(e) {
 		method: 'PUT',
 		body: data
 	})
-	.then((res) => res.json())
-	.then((data) => {
-		welcDiv.style.display = data.welcDivApp;
-		wText.innerHTML = data.welcText;
-		formDiv.style.display = data.formApp;
-		//welcDiv.style.display = data.;
-	})
+		.then((res) => res.json())
+		.then((data) => {
+			welcDiv.style.display = data.welcDivApp;
+			wText.innerHTML = data.welcText;
+			formDiv.style.display = data.formApp;
+		})
 }
 
-onOff.onclick = function(e) {
+onOff.onclick = function (e) {
 	e.preventDefault(e);
 	let on;
 	let drOpened;
-	if(onOff.classList.contains('on')) {
+	if (onOff.classList.contains('on')) {
 		on = true;
 	} else {
 		on = false;
 	}
-	if(drum.classList.contains('open')) {
+	if (drum.classList.contains('open')) {
 		drOpened = true;
 	} else {
 		drOpened = false;
 	}
-
 	fetch('/switchDevice', {
 		headers: {
 			'Accept': 'application/json',
 			'Content-Type': 'application/json'
 		},
 		method: 'PUT',
-		body: JSON.stringify({switchedOn: on, drumOpened: drOpened })
+		body: JSON.stringify({ switchedOn: on, drumOpened: drOpened })
 	})
-	.then((res) => res.json())
-	.then((data) => {
-		if(data.notLogged) {
-			alert('Please, log in to use the device!');
-		} else {
-			console.log(data.switchClass + " " + data.drumClass);
-			if(!data.switchClass) {
-				//alert('s');
-				onOff.classList.remove(data.on);
-				drum.classList.add(data.open);
+		.then((res) => res.json())
+		.then((data) => {
+			if (data.notLogged) {
+				alert('Please, log in to use the device!');
 			} else {
-				onOff.classList.add(data.on);
-				drum.classList.remove(data.open);
+				//console.log(data.switchClass + " " + data.drumClass);
+				if (!data.switchClass) {
+					onOff.classList.remove(data.on);
+					drum.classList.add(data.open);
+				} else {
+					onOff.classList.add(data.on);
+					drum.classList.remove(data.open);
+				}
 			}
-		}
-	})
+		})
 }
 
-logOut.onclick = function(e) {
+logOut.onclick = function (e) {
 	e.preventDefault();
 
 	fetch('/logOut', {
@@ -88,138 +82,102 @@ logOut.onclick = function(e) {
 			'Accept': 'application/json',
 			'Content-Type': 'application/json'
 		},
-		method: 'GET',
-		//body: JSON.stringify({logOut: true})
+		method: 'GET'
 	})
-	.then((res) => res.json())
-	.then((data) => {
-		alert('Bye, ' + data.userName);
-		let tbody = document.getElementsByTagName('tbody')[0];
-		if(tbody) {
-			modesTable.removeChild(tbody);
-		}
-		welcDiv.style.display = data.welcDivApp;
-		wText.innerHTML = '';
-		formDiv.style.display = data.formApp;
-		chooseModes.style.display = data.modesDivApp;
-		addModeDiv.style.display = data.addModeApp;
-		changeModeDiv.style.display = data.changeModeApp;
-	})
+		.then((res) => res.json())
+		.then((data) => {
+			alert('Bye, ' + data.userName);
+			let tbody = document.getElementsByTagName('tbody')[0];
+			if (tbody) {
+				modesTable.removeChild(tbody);
+			}
+			welcDiv.style.display = data.welcDivApp;
+			wText.innerHTML = '';
+			formDiv.style.display = data.formApp;
+			chooseModes.style.display = data.modesDivApp;
+			addModeDiv.style.display = data.addModeApp;
+			changeModeDiv.style.display = data.changeModeApp;
+		})
 }
 
-/*button.onclick = function(e) {
+switchModes.onclick = function (e) {
+	e.preventDefault();
+	fetch('/api/getModes', {
+		headers: {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json'
+		},
+		method: 'GET'
+	})
+		.then((res) => res.json())
+		.then((data) => {
+			if (data.notLogged) {
+				alert('Please, log in to use the device!');
+			} else {
+				if (data.addModeApp) {
+					addModeDiv.style.display = data.addModeApp;
+				}
+				chooseModes.style.display = data.modesDivApp;
+			}
+		})
+}
+
+addModeClose.onclick = function (e) {
+	e.preventDefault();
+	fetch('/api/getModes', {
+		headers: {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json'
+		},
+		method: 'GET'
+	})
+		.then((res) => res.json())
+		.then((data) => {
+
+			if (data.addModeApp) {
+				addModeDiv.style.display = data.addModeApp;
+			}
+			chooseModes.style.display = data.modesDivApp;
+
+		})
+}
+
+closeAddModeForm.onclick = function (e) {
 	e.preventDefault();
 	fetch('/api/closeCreateMode', {
 		headers: {
 			'Accept': 'application/json',
 			'Content-Type': 'application/json'
 		},
-		method:'GET',
-		//body: JSON.stringify({display: chooseModes.style.display})
+		method: 'GET'
 	})
-	.then((res) => res.json())
-	.then((data) => {
-		addModeDiv.style.display = data.addModeApp;
-	});
-}*/
-
-switchModes.onclick = function(e) {
-	e.preventDefault();
-	//alert('sss');
-	//let display = chooseModes.style.display;
-	fetch('/api/getModes', {
-		headers: {
-			'Accept': 'application/json',
-			'Content-Type': 'application/json'
-		},
-		method:'GET',
-		//body: JSON.stringify({display: chooseModes.style.display})
-	})
-	.then((res) => res.json())
-	.then((data) => {
-		if(data.notLogged) {
-			alert('Please, log in to use the device!');
-		} else {
-			if(data.addModeApp) {
-				addModeDiv.style.display = data.addModeApp;
-			}
-			chooseModes.style.display = data.modesDivApp;
-		}
-	})
-}
-addModeClose.onclick = function(e) {
-	e.preventDefault();
-	fetch('/api/getModes', {
-		headers: {
-			'Accept': 'application/json',
-			'Content-Type': 'application/json'
-		},
-		method:'GET',
-		//body: JSON.stringify({display: chooseModes.style.display})
-	})
-	.then((res) => res.json())
-	.then((data) => {
-		
-		if(data.addModeApp) {
+		.then((res) => res.json())
+		.then((data) => {
 			addModeDiv.style.display = data.addModeApp;
-		}
-		chooseModes.style.display = data.modesDivApp;
-	
-	})
-};
-closeAddModeForm.onclick = function(e) {
-    e.preventDefault();
-    fetch('/api/closeCreateMode', {
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        method:'GET',
-        //body: JSON.stringify({display: chooseModes.style.display})
-    })
-    .then((res) => res.json())
-    .then((data) => {
-        addModeDiv.style.display = data.addModeApp;
-    });
+			addModeForm.modeName.value = "";
+
+		});
 }
-openAddMode.onclick = function(e) {
+
+openAddMode.onclick = function (e) {
 	e.preventDefault();
 	fetch('/api/addModeMenu', {
 		headers: {
 			'Accept': 'application/json',
 			'Content-Type': 'application/json'
 		},
-		method:'GET'
-		//body: JSON.stringify({chooseModes: true})
+		method: 'GET'
 	})
-	.then((res) => res.json())
-	.then((data) => {
-		addModeDiv.style.display = data.addModeApp;
-		changeModeDiv.style.display = data.changeModeApp
-	})
+		.then((res) => res.json())
+		.then((data) => {
+			addModeDiv.style.display = data.addModeApp;
+			changeModeDiv.style.display = data.changeModeApp
+		})
 }
 
-/*addModeF.spin.onchange = function(e) {
+addModeForm.onsubmit = function (e) {
 	e.preventDefault();
-	valSpin.innerHTML = addModeF.spin.value;
-}
-addModeF.temp.onchange = function(e) {
-	e.preventDefault();
-	valTemp.innerHTML = addModeF.temp.value;
-}*/
-/*changeModeF.spin.onchange = function(e) {
-	e.preventDefault();
-	valSpin.innerHTML = changeModeF.spin.value;
-}
-changeModeF.temp.onchange = function(e) {
-	e.preventDefault();
-	valTemp.innerHTML = changeModeF.temp.value;
-}*/
-
-addModeF.onsubmit = function(e) {
-	e.preventDefault();
-	//alert('trying to add mode');
-	let data = JSON.stringify(formDataToJSON(addModeF));
+	let data = JSON.stringify(formDataToJSON(addModeForm));
 	fetch('/api/createMode', {
 		headers: {
 			'Accept': 'application/json',
@@ -228,80 +186,77 @@ addModeF.onsubmit = function(e) {
 		method: 'POST',
 		body: data
 	})
-	.then((res) => res.json())
-	.then((data) => {
-		let tr;
-		let td;
-		let buttonIdObj = {
-			'changeMode': 'change Mode',
-			'deleteMode': 'delete Mode'
-		}
-		addModeF.modeName.value = '';
-		//console.log(data);
-		let tbody;
-		if(!modesTable.getElementsByTagName('tbody')[0]) {
-			tbody = document.createElement('tbody');
-			modesTable.appendChild(tbody);
-		} else {
-			tbody = modesTable.getElementsByTagName('tbody')[0];
-		}
-		tr = document.createElement('tr');
-		//console.log(data.id);
-		//tr.id = data.id;
-		let modeObj = data.modes[data.modes.length - 1]
-		tr.id = modeObj.id;
-		
-		for(let key in modeObj) {
-			if(key == 'id') continue;
-			td = document.createElement('td');
-			td.innerHTML = modeObj[key];
-			tr.appendChild(td);
-		}
-		let button;
-		for(let key in buttonIdObj) {
-			td = document.createElement('td');
-			button = document.createElement('button');
-			button.classList.add(key);
-			button.innerHTML = buttonIdObj[key];
-			//button.addEventListener("click", function() {alert('ff');}, false);
-			td.appendChild(button);
-			tr.appendChild(td);
-		}
-		tbody.appendChild(tr);
-		let changeModeButtons = document.getElementsByClassName('changeMode');
-		let lastChangeModeButton = changeModeButtons[changeModeButtons.length - 1];
-		lastChangeModeButton.addEventListener('click',
-			changeMode, false);
-		let deleteModeButtons = document.getElementsByClassName('deleteMode');
-		let lastDeleteModeButton = deleteModeButtons[deleteModeButtons.length - 1];
-		lastDeleteModeButton.addEventListener('click',
-			deleteMode, false);
+		.then((res) => res.json())
+		.then((data) => {
+			let tr;
+			let td;
+			let buttonIdObj = {
+				'changeMode': 'change Mode',
+				'deleteMode': 'delete Mode'
+			}
+			addModeForm.modeName.value = '';
+			let tbody;
+			if (!modesTable.getElementsByTagName('tbody')[0]) {
+				tbody = document.createElement('tbody');
+				modesTable.appendChild(tbody);
+			} else {
+				tbody = modesTable.getElementsByTagName('tbody')[0];
+			}
+			tr = document.createElement('tr');
+			let modeObj = data.modes[data.modes.length - 1]
+			tr.id = modeObj.id;
+
+			for (let key in modeObj) {
+				if (key == 'id') continue;
+				td = document.createElement('td');
+				td.innerHTML = modeObj[key];
+				tr.appendChild(td);
+			}
+			let button;
+			for (let key in buttonIdObj) {
+				td = document.createElement('td');
+				button = document.createElement('button');
+				button.classList.add(key);
+				button.innerHTML = buttonIdObj[key];
+				td.appendChild(button);
+				tr.appendChild(td);
+			}
+			tbody.appendChild(tr);
+			let changeModeButtons = document.getElementsByClassName('changeMode');
+			let lastChangeModeButton = changeModeButtons[changeModeButtons.length - 1];
+			lastChangeModeButton.addEventListener('click',
+				changeMode, false);
+			let deleteModeButtons = document.getElementsByClassName('deleteMode');
+			let lastDeleteModeButton = deleteModeButtons[deleteModeButtons.length - 1];
+			lastDeleteModeButton.addEventListener('click',
+				deleteMode, false);
 			lastDeleteModeButton.classList.add("red");
-		addModeDiv.style.display = data.addModeApp;
-	})
+			addModeDiv.style.display = data.addModeApp;
+		})
 }
+
 function deleteMode() {
 	let modeId = this.parentNode.parentNode.id;
 	let conf = confirm('Are you sure?');
-		if(conf) {
-			fetch('/api/deleteMode', {
-				headers: {
-					'Accept': 'application/json',
-					'Content-Type': 'application/json'
-				},
-				method:'DELETE',
-				body: JSON.stringify({modeId: modeId})
-			})
+	if (conf) {
+		fetch('/api/deleteMode', {
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			},
+			method: 'DELETE',
+			body: JSON.stringify({ modeId: modeId })
+		})
 			.then((res) => res.json())
 			.then((data) => {
-				if(data.success) {
+				if (data.success) {
 					let trToDelete = document.getElementById(modeId);
 					trToDelete.parentNode.removeChild(trToDelete);
-					//alert('The mode has been successfully deleted');
 				}
 			})
-		}
+	}
 }
+
 function changeMode() {
 	let modeId = this.parentNode.parentNode.id;
 	fetch('/api/changeModeMenu?modeId=' + modeId, {
@@ -309,22 +264,19 @@ function changeMode() {
 			'Accept': 'application/json',
 			'Content-Type': 'application/json'
 		},
-		method:'GET',
-		//body: JSON.stringify({modeId: modeId})
+		method: 'GET'
 	})
-	.then((res) => res.json())
-	.then((data) => {
-		//let modeToChange = data.modeToChange;
-		changeModeDiv.style.display = data.changeModeApp;
-		addModeDiv.style.display = data.addModeApp;
-		changeModeF.modeName.value = data.modeName;
-		changeModeF.spin.value = data.spin;
-		changeModeF.temp.value = data.temp;
-	})
-
-
+		.then((res) => res.json())
+		.then((data) => {
+			changeModeDiv.style.display = data.changeModeApp;
+			addModeDiv.style.display = data.addModeApp;
+			changeModeF.modeName.value = data.modeName;
+			changeModeF.spin.value = data.spin;
+			changeModeF.temp.value = data.temp;
+		})
 }
-changeModeF.onsubmit = function(e) {
+
+changeModeF.onsubmit = function (e) {
 	e.preventDefault();
 	let data = JSON.stringify(formDataToJSON(changeModeF), {});
 	fetch('/api/changeMode', {
@@ -335,27 +287,25 @@ changeModeF.onsubmit = function(e) {
 		method: 'PUT',
 		body: data
 	})
-	.then((res) => res.json())
-	.then((data) => {
-		changeModeDiv.style.display = data.changeModeApp;
-		let trCells = document.getElementById(data.modeToChange.id).cells;
-		console.dir(trCells)
-		let i = 0; 
-		for(let key in data.modeToChange) {
-			if(key == 'id') continue;
-			trCells[i].innerHTML = data.modeToChange[key];
-			i++;
-		}
-
-	})
+		.then((res) => res.json())
+		.then((data) => {
+			changeModeDiv.style.display = data.changeModeApp;
+			let trCells = document.getElementById(data.modeToChange.id).cells;
+			console.dir(trCells)
+			let i = 0;
+			for (let key in data.modeToChange) {
+				if (key == 'id') continue;
+				trCells[i].innerHTML = data.modeToChange[key];
+				i++;
+			}
+		})
 }
 
-function formDataToJSON(formElement){    
-    var formData = new FormData(formElement), ConvertedJSON= {};
-    for (const [key, value]  of formData.entries())
-    {
-        ConvertedJSON[key] = value;
-    }
+function formDataToJSON(formElement) {
+	var formData = new FormData(formElement), ConvertedJSON = {};
+	for (const [key, value] of formData.entries()) {
+		ConvertedJSON[key] = value;
+	}
 
-    return ConvertedJSON;
+	return ConvertedJSON;
 }
